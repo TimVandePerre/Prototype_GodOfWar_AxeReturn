5ac0dc92942f944e77dfd51b6673c4c9432c3951
TimVandePerre <tim.vdp1911@hotmail.com>
2024-07-30 11:57:37 +0200

    Basic third person movement movement
    Using the free look camera from cinemamachine plugin and a character controller.
    The Movement works on both pc and controller inputs.

--- Assets/Scripts/Player/MovementScript.cs
+++ Assets/Scripts/Player/MovementScript.cs
@@ -1,25 +1,31 @@
+using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MovementScript : MonoBehaviour
 {
-    private PlayerInput _playerInput;
-    private InputAction Player_Movement;
+    private PlayerInputs _inputs;
+    private InputAction _player_Movement;
 
+    private CharacterController _characterController;
     //Speed of the player.
     [SerializeField] private float Player_MovementSpeed;
+    [SerializeField] private Camera Cam;
 
     private void OnEnable()
     {
-        Player_Movement.Enable();
+        _player_Movement = _inputs.Movement.Move;
+        _player_Movement.Enable();
     }
 
-    void Start()
+    private void Awake()
     {
-
+        _inputs = new PlayerInputs();
+        _characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -30,20 +36,23 @@ public class MovementScript : MonoBehaviour
 
     private void UpdateMovement()
     {
-        //Update the direction based on the input value
-        Vector2 Move = Player_Movement.ReadValue<Vector2>() * Player_MovementSpeed;
+        //Get the input
+        Vector2 Input = _player_Movement.ReadValue<Vector2>();
 
-        Debug.Log(Player_Movement.ReadValue<Vector2>());
-        Vector3 currentPos = transform.position;
-        
-        currentPos.x += Move.x;
-        currentPos.z += Move.y;
+        //update movement based on rotation.
+        Vector3 camRigthInput = Cam.transform.right * Input.x;
+        Vector3 camForwardInput = Cam.transform.forward * Input.y;
+        camForwardInput.y = 0; //otherwise the movement is also going to affect the height of the player.
 
-        Vector3.Lerp(transform.position, currentPos, Player_MovementSpeed);
+        //calculat the direction the player is moving based on the input and camera.
+        Vector3 direction = camForwardInput + camRigthInput;
+        //direction.y = _velocity; To be added when using jumping or gravity.
+
+        _characterController.Move(direction * Player_MovementSpeed * Time.deltaTime);
     }
 
     private void OnDisable()
     {
-        Player_Movement.Disable();
+        _player_Movement.Disable();
     }
 }
